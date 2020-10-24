f9c79d4925415e6bca23c037b88d56adc04221ac
Thomas Lian Ødegaard <13934813+thomaslian@users.noreply.github.com>
2020-10-24 16:56:06 +0200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	let promise = getDoUsername();
+	let darkmode = false;
 
 	async function getDoUsername() {
 		const res = await fetch(`https://thatcopy.pw/do/`);
@@ -15,6 +16,38 @@
 	function handleClick() {
 		promise = getDoUsername();
 	}
+
+	function setDarkMode() {
+		const headings = document.getElementsByTagName("h1");
+		const buttons = document.getElementsByTagName("button");
+		if (!darkmode) {
+			darkmode = true;
+			document.body.style.background = '#2E2C2F';
+			for (let heading of headings) {
+				if (heading.id != "error") heading.style.color = 'white';
+			}
+			for (let button of buttons) {
+    			if (button.id == "darkmode") {
+					button.innerHTML = "Darkmode: On";
+				}
+				button.style.background = '#475B63';
+				button.style.color = 'white';
+			}
+		} else {
+			darkmode = false;
+			document.body.style.background = 'white';
+			for (let heading of headings) {
+				if (heading.id != "error") heading.style.color = 'black';
+			}
+			for (let button of buttons) {
+				if (button.id == "darkmode") {
+					button.innerHTML = "Darkmode: Off";
+				}
+				button.style.background = '#f4f4f4';
+				button.style.color = 'black';
+			}
+		}
+	}
 </script>
 
 <main>
@@ -28,9 +61,12 @@
 	{:then username}
 		<h1>{username}</h1>
 	{:catch error}
-		<h1 style="color: red">{error.message}</h1>
+		<h1 id="error" style="color: red">{error.message}</h1>
 	{/await}
 	<img id="sammy" src="/sammy.webp" alt="Sammy">
+	<button id="darkmode" on:click={setDarkMode}>
+		Darkmode: Off
+	</button>
 </div>
 </main>
 
@@ -48,6 +84,7 @@
   		margin-left: auto;
   		margin-right: auto;
   		width: 50%;
+		margin-bottom: 24px;
 	}
 	h1{
 		text-align: center;
